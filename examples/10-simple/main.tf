ac2689ba65b1e30dee231c03c82d79b7ab109e4d
Joerg Kritzen <j.kritzen@ontracon.de>
2022-09-30 10:01:48 +0200

    fix unknown provider

--- examples/10-simple/main.tf
+++ examples/10-simple/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "~> 1"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
