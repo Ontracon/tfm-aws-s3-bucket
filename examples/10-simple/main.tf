7eb0e6b1900ebb41b73e663c19788d97d04f64d4
Joerg Kritzen <j.kritzen@ontracon.de>
2022-10-05 19:19:35 +0200

    add aws provider

--- examples/10-simple/main.tf
+++ examples/10-simple/main.tf
@@ -1,10 +1,14 @@
 terraform {
   required_version = "~> 1"
-
+  backend "local" {}
   required_providers {
     random = {
       source  = "hashicorp/random"
       version = "~> 3"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4"
     }
   }
 }
